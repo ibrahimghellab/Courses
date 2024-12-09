e6715e4151984329bd4ec1f02a07500092cabb02
ibrahimghellab <ghellabibrahim@gmail.com>
2024-12-09 22:37:39 +0100

    Add form for modifying vehicle details in formulaireModification.php

---
+++ BUT2/PHP/TP11/ex6/vue/voiture/formulaireModification.php
@@ -0,0 +1,19 @@
+<main>
+    <form method="get" action="routeur.php">
+        <input type="hidden" name="controleur" value="controleurVoiture">
+        <input type="hidden" name="action" value="modifierVoiture">
+        <div>
+            <label for="log">immatriculation</label>
+            <input type="text" name="immatriculation" value="<?php echo $i; ?>" readonly>
+        </div>
+        <div>
+            <label for="marque">marque</label>
+            <input type="text" name="marque" value="<?php echo $m; ?>">
+        </div>
+        <div>
+            <label for="couleur">couleur</label>
+            <input type="text" name="couleur" value="<?php echo $c; ?>">
+        </div>
+        <button type="submit">Modifier</button >
+    </form>
+</main>
