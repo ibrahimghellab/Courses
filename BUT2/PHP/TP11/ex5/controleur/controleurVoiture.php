69abb6ea62af3ca67a4098762d0dbb060457ed37
ibrahimghellab <ghellabibrahim@gmail.com>
2024-12-09 22:37:59 +0100

    Add edit functionality for vehicle details in controleurVoiture.php

--- BUT2/PHP/TP11/ex5/controleur/controleurVoiture.php
+++ BUT2/PHP/TP11/ex5/controleur/controleurVoiture.php
@@ -1,6 +1,7 @@
 <?php
 require_once("controleur.php");
 require_once(__DIR__."/../modele/voiture.php");
+require_once("./modele/modele.php");
 
 class controleurVoiture extends Controleur{
     protected static $objet="voiture";
@@ -8,6 +9,7 @@ class controleurVoiture extends Controleur{
     protected static $key="immatriculation";
     protected static $delete="supprimerObjet&immatriculation=";
     protected static $read="lireObjet&immatriculation=";
+    protected static $edit="afficherFormulaireModification&immatriculation=";
     protected static $link='<a href=routeur.php?controleur=controleurVoiture&action=';
     protected static $txt='<br>voiture immatriculée ';
     protected static $errorpart1="<p>la voiture immatriculée ";
@@ -37,8 +39,14 @@ class controleurVoiture extends Controleur{
 
     public static function afficherFormulaireModification(){
         $i=$_GET["immatriculation"];
-        $voiture=self::getObjetById($i);
-        require_once(__DIR__."/../vue/voiture/formulaireModification.html");
+        $voiture=Voiture::getObjetById($i);
+        $i=$voiture->get("immatriculation");
+        $m=$voiture->get("marque");
+        $c=$voiture->get("couleur"); 
+        require_once(__DIR__."/../vue/debut.php");
+        require_once(__DIR__."/../vue/menu.html");
+        require_once(__DIR__.'/../vue/voiture/formulaireModification.php');
+        require_once(__DIR__."/../vue/fin.html");
         
     }
 }
